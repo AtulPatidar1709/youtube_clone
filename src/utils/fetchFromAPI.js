3b3ede8fcf0a100002de7b8640d838f657a97f59
AtulPatidar <atulpatidar1709@gmail.com>
2022-09-13 17:27:59 +0530

    Change to old Key

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': "26c1be505bmsh16a81b6c62b2ed1p112791jsn17bf434b65cf",
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
